1daf7e8f89485f20943734b9841c8468b7c52363
Lelik Jan <21alex7777@gmail.com>
2023-03-06 23:00:12 +0300

    feat: ts lection part two

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+
+
 #TEST TS
 
+### sources:
++ [Node.js 2021Q2 Typescript 26.05.21 (part 1) ]( https://www.youtube.com/watch?v=I_aTbZcH8Do)
+
++ [Node.js 2021Q2 Typescript part 2 ](https://www.youtube.com/watch?v=CegrbRXGw20)
+
+## Steps:
+
 * create in bash dir testTs
-* npm init
+* npm init -y
 * npm i --save-dev nodemon ts-node typescript
 
 nodemon - allow automatically reload app, like a live server
@@ -19,6 +28,7 @@ ts-node - it transforms ts into js and allow  working  node with ts
   },
 ```
 build - create index.js
+
 tsc - invoke ts compiler
 * create nodemon.json
 ```
--- index.ts
+++ index.ts
@@ -284,3 +284,204 @@ const dictionary: IDictionary = {
 
 // non null assertion syntax -if we are sure it is not null or undefined then use !
 //const myDiv =document.getElementById('my-div')!;
+
+
+//classes
+
+class Person {
+    name: string;
+    age: number | null;
+
+    constructor(name = 'anonymous', age: number | null = null) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi(): void {
+        console.log(`hi, ${this.name}`);
+    }
+}
+
+const person = new Person();
+//person.name = 'tom'
+//person.age = 23;
+console.log(person);
+person.sayHi();
+
+const person2 = new Person('bob', 44);
+console.log(person2);
+
+//classes and interfaces
+//interface dictates which fields and methods must be mandatory for the class in which the interface is implemented
+//also we can add another fields and methods in class  yet
+interface IPersonForClass {
+    name: string;
+    age: number | null;
+    sayHi: () => void;
+}
+
+class Person2 implements IPersonForClass {
+    name: string;
+    age: number | null;
+    human: boolean;
+
+    constructor(name = 'tommy', age = null, human = true) {
+        this.name = name;
+        this.age = age;
+        this.human = human;
+    }
+
+    sayHi() {
+        console.log(`hi, ${this.name}`);
+    }
+
+// strangely it works!!!!!!
+    sayHiArrow = (): void => {
+        console.log(`hi,${this.name}`);
+    }
+}
+
+const person3 = new Person2();
+console.log(person3);
+person3.sayHiArrow();//hi,tommy
+
+//abstract classes
+abstract class AbstractGreeting {
+    abstract sayHi(): void;
+}
+
+class MyGreeting extends AbstractGreeting {
+    sayHi() {
+        console.log('hi');
+    }
+}
+
+//Property 'sayHi' in type 'MyGreeting2' is not assignable to the same property in base type 'AbstractGreeting'.   Type '(msg: any) => void' is not assignable to type '() => void'.
+// class MyGreeting2 extends AbstractGreeting{
+//     sayHi(msg) {
+//         console.log(msg);
+//     }
+// }
+
+//class members visibility : public, protected(available in the child classes),private(only in current class)
+//in child classes can override protected to public
+//all these visibility modifiers exist only in ts and in js do not work
+
+//generics
+
+function identity<T>(value: T): T {
+    return value;
+}
+
+//generic interface
+//example of polymorphism
+interface IMyStorage<T> {
+    value: T[];
+    getValue: () => T[];
+}
+
+const numericStorage: IMyStorage<number> = {
+    value: [],
+    getValue() {
+        return this.value;
+    }
+}
+const booleanStorage: IMyStorage<boolean> = {
+    value: [],
+    getValue() {
+        return this.value;
+    }
+}
+
+//generic class
+class IdentifyClass<T> {
+    value: T;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+
+    getIdentify(): T {
+        return this.value;
+    }
+}
+
+const numericIdentify: IdentifyClass<number> = new IdentifyClass(5);
+const stringIdentify: IdentifyClass<string> = new IdentifyClass('hello');
+
+//type manipulations
+
+//keyof  is used to extract the key type from an object type
+type Rectangle = {
+    width: number;
+    height: number;
+    type: string;
+}
+const myRectangle: Rectangle = {
+    width: 10,
+    height: 10,
+    type: 'box',
+}
+const getValueFromRectProp = (prop: keyof Rectangle, rectangle: Rectangle) => rectangle[prop];
+console.log(getValueFromRectProp('width', myRectangle));
+console.log(getValueFromRectProp('type', myRectangle));
+
+//typeof
+function abc() {
+    return {
+        a: 10,
+        b: 'hello',
+    }
+}
+
+type ABC = ReturnType<typeof abc>
+const abc2: typeof abc = () => {
+    return {
+        a: 2,
+        b: 'world',
+    }
+}
+
+//indexed access types
+type PesonAny = {
+    name: string;
+    age: number;
+}
+type Age = PesonAny['age'];
+const ageAny: Age = 34;
+
+//conditional types
+interface Animal {
+    live(): void;
+}
+
+interface Dog extends Animal {
+    woof(): void;
+}
+
+type Type1 = Dog extends Animal ? number : string;
+const value: Type1 = 33;
+
+//mapped types
+type StrNum = {
+    [key: string]: number | string;
+}
+const obj22: StrNum = {
+    hello: 'hello',
+    test: 123,
+    //Type 'boolean' is not assignable to type 'string | number'.
+    // isTrue:false,
+}
+
+//mapped types with literals
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+type DirectionObj = {
+    [key in Direction]: boolean;
+}
+const directionUsed: DirectionObj = {
+    left: true,
+    right: false,
+    up: true,
+    down: false,
+}
